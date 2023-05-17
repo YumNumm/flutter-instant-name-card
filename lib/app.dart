34396a0ae1388d343a2f4e3da9dec1b870ace30f
YumNumm <onory0917@gmail.com>
2023-05-17 23:30:44 +0900

    add: theme

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_name_card_printer/feature/common/theme/theme.dart';
+import 'package:flutter_name_card_printer/feature/home/screen/home_screen.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      theme: lightTheme,
+      darkTheme: darkTheme,
+      home: const HomeScreen(),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_name_card_printer/app.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 Future<void> main() async {
-  runApp(const App());
+  runApp(
+    const ProviderScope(
+      child: App(),
+    ),
+  );
 }

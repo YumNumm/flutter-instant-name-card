5ba20e5d4860b08780816277e5f9e442ab93aa95
YumNumm <onory0917@gmail.com>
2023-05-18 13:49:52 +0900

    add: images

--- lib/feature/home/screen/home_screen.dart
+++ lib/feature/home/screen/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_name_card_printer/feature/home/viewmodel/home_screen_viewmodel.dart';
+import 'package:flutter_name_card_printer/feature/input/model/name_card_model.dart';
+import 'package:flutter_name_card_printer/feature/input/viewmodel/input_view_model.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class HomeScreen extends ConsumerWidget {
@@ -18,14 +20,91 @@ class HomeScreen extends ConsumerWidget {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          ListTile(
-            title: const Text('名刺の内容を入力する'),
-            leading: const Icon(Icons.input),
-            onTap: ref.read(homeScreenViewModelProvider).navigateToInputScreen,
-          ),
-        ],
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: ref.read(inputViewModelProvider.notifier).printNameCard,
+        label: const Text('印刷'),
+        icon: const Icon(Icons.print),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ListTile(
+              title: const Text('名刺の内容を入力する'),
+              leading: const Icon(Icons.input),
+              onTap:
+                  ref.read(homeScreenViewModelProvider).navigateToInputScreen,
+            ),
+            NameCardInformationWidget(
+              nameCard: ref.watch(inputViewModelProvider),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class NameCardInformationWidget extends StatelessWidget {
+  const NameCardInformationWidget({required this.nameCard, super.key});
+
+  final NameCardModel nameCard;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.all(10),
+      elevation: 0,
+      shape: RoundedRectangleBorder(
+        side: BorderSide(
+          color: Theme.of(context).colorScheme.outline,
+        ),
+        borderRadius: const BorderRadius.all(Radius.circular(12)),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Row(),
+            Text('名前: ${nameCard.name}'),
+            Text('名前(サブ): ${nameCard.nameSub ?? ""}'),
+            const Divider(),
+            Text('所属組織: ${nameCard.organization ?? ""}'),
+            Text('役職: ${nameCard.position ?? ""}'),
+            if (nameCard.organizationIconUrl != null)
+              Center(
+                child: Card(
+                  elevation: 0,
+                  color: Theme.of(context)
+                      .colorScheme
+                      .onPrimaryContainer
+                      .withOpacity(0.1),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: Image.network(
+                      nameCard.organizationIconUrl!,
+                      width: 100,
+                      height: 100,
+                      errorBuilder: (_, __, ___) => const Column(
+                        children: [
+                          Icon(Icons.error),
+                          Text('組織アイコンの取得に失敗しました。')
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            const Divider(),
+            Text('電話番号: ${nameCard.phoneNumber ?? ""}'),
+            Text('メールアドレス: ${nameCard.email ?? ""}'),
+            Text('Webサイト: ${nameCard.website ?? ""}'),
+            const Divider(),
+            const Text('SNS'),
+            for (final snsAccount in nameCard.snsAccounts)
+              Text('${snsAccount.type.name}: ${snsAccount.name}'),
+          ],
+        ),
       ),
     );
   }
--- lib/feature/input/viewmodel/input_view_model.dart
+++ lib/feature/input/viewmodel/input_view_model.dart
@@ -1,9 +1,18 @@
 import 'dart:convert';
+import 'dart:developer';
+import 'dart:io';
 
+import 'package:flutter/services.dart';
 import 'package:flutter_name_card_printer/feature/common/provider/shared_preferences.dart';
 import 'package:flutter_name_card_printer/feature/input/model/name_card_model.dart';
+import 'package:flutter_name_card_printer/feature/input/model/sns_account_model.dart';
+import 'package:image/image.dart' as Img;
+import 'package:intl/intl.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:sunmi_printer_plus/enums.dart';
+import 'package:sunmi_printer_plus/sunmi_printer_plus.dart';
+import 'package:sunmi_printer_plus/sunmi_style.dart';
 
 part 'input_view_model.g.dart';
 
@@ -12,6 +21,7 @@ class InputViewModel extends _$InputViewModel {
   @override
   NameCardModel build() {
     prefs = ref.read(sharedPreferencesProvider);
+    ref.listenSelf((_, next) => _saveInputState(next));
     return _loadInputState();
   }
 
@@ -29,5 +39,191 @@ class InputViewModel extends _$InputViewModel {
     return const NameCardModel();
   }
 
+  void _saveInputState(NameCardModel state) {
+    prefs.setString(_prefsKey, jsonEncode(state.toJson()));
+  }
+
   void setName(String name) => state = state.copyWith(name: name);
+  void setNameSub(String nameSub) => state = state.copyWith(nameSub: nameSub);
+  void setOrganization(String organization) =>
+      state = state.copyWith(organization: organization);
+  void setOrganizationIconUrl(String organizationIconUrl) =>
+      state = state.copyWith(organizationIconUrl: organizationIconUrl);
+  void setPosition(String position) =>
+      state = state.copyWith(position: position);
+  void setPhoneNumber(String phoneNumber) =>
+      state = state.copyWith(phoneNumber: phoneNumber);
+  void setEmail(String email) => state = state.copyWith(email: email);
+  void setWebsite(String website) => state = state.copyWith(website: website);
+  void setSnsAccounts(List<SnsAccountModel> snsAccounts) =>
+      state = state.copyWith(snsAccounts: snsAccounts);
+  void updateSnsAccount(SnsAccountModel snsAcount) {
+    final index = state.snsAccounts
+        .indexWhere((element) => element.type == snsAcount.type);
+    if (index >= 0) {
+      final newSnsAccounts = state.snsAccounts;
+      newSnsAccounts[index] = snsAcount;
+      state = state.copyWith(snsAccounts: newSnsAccounts);
+    }
+    state = state.copyWith(snsAccounts: [...state.snsAccounts, snsAcount]);
+  }
+
+  Future<void> exportAsQRCode() async {
+    final data = jsonEncode(state);
+    // 圧縮する
+    final compressedData = base64Encode(
+      gzip.encode(
+        utf8.encode(data),
+      ),
+    );
+    await SunmiPrinter.lineWrap(2);
+    await SunmiPrinter.printQRCode(
+      compressedData,
+      errorLevel: SunmiQrcodeLevel.LEVEL_L,
+    );
+    await SunmiPrinter.printText(
+      "発行: ${DateFormat('yyyy/MM/dd HH:mm:ss').format(DateTime.now())}",
+      style: SunmiStyle(
+        bold: true,
+      ),
+    );
+    await SunmiPrinter.lineWrap(2);
+  }
+
+  void loadFromQrString(String qrString) {
+    final data = jsonDecode(
+      utf8.decode(
+        gzip.decode(
+          base64Decode(qrString),
+        ),
+      ),
+    ) as Map<String, dynamic>;
+    state = NameCardModel.fromJson(data);
+  }
+
+  Future<void> printNameCard() async {
+    await SunmiPrinter.initPrinter();
+    await SunmiPrinter.lineWrap(2);
+    // 名前
+    await SunmiPrinter.printText(
+      state.name,
+      style: SunmiStyle(
+        align: SunmiPrintAlign.CENTER,
+        bold: true,
+        fontSize: SunmiFontSize.LG,
+      ),
+    );
+    if (state.nameSub != null) {
+      await SunmiPrinter.printText(
+        state.nameSub!,
+        style: SunmiStyle(
+          align: SunmiPrintAlign.CENTER,
+          fontSize: SunmiFontSize.MD,
+          bold: true,
+        ),
+      );
+    }
+    // line
+    await SunmiPrinter.line();
+
+    // 組織
+    if (state.organization != null) {
+      await SunmiPrinter.printText(
+        state.organization!,
+        style: SunmiStyle(
+          align: SunmiPrintAlign.RIGHT,
+          fontSize: SunmiFontSize.MD,
+          bold: true,
+        ),
+      );
+    }
+    // 組織Image
+    if (state.organizationIconUrl != null &&
+        state.organizationIconUrl!.isNotEmpty) {
+      log('message');
+      await SunmiPrinter.startTransactionPrint();
+      final url = state.organizationIconUrl!;
+      final byte = (await NetworkAssetBundle(Uri.parse(url)).load(url))
+          .buffer
+          .asUint8List();
+      final cmd = Img.Command()
+        ..decodeImage(byte)
+        ..grayscale(
+          amount: 10,
+        )
+        ..copyResize(
+          width: (384 * 0.5).toInt(),
+        )
+        ..encodeJpg();
+
+      final image = await cmd.getBytes();
+
+      await SunmiPrinter.setAlignment(SunmiPrintAlign.RIGHT);
+      await SunmiPrinter.printImage(
+        image!,
+      );
+      await SunmiPrinter.lineWrap(1);
+      await SunmiPrinter.exitTransactionPrint();
+    }
+    // 職位
+    if (state.position != null) {
+      await SunmiPrinter.printText(
+        state.position!,
+        style: SunmiStyle(
+          align: SunmiPrintAlign.CENTER,
+          fontSize: SunmiFontSize.MD,
+          bold: true,
+        ),
+      );
+    }
+
+    // line
+    await SunmiPrinter.line();
+
+    // 電話番号
+    if (state.phoneNumber != null) {
+      await SunmiPrinter.printText(
+        'TEL: ${state.phoneNumber!}',
+        style: SunmiStyle(
+          fontSize: SunmiFontSize.MD,
+          bold: true,
+        ),
+      );
+    }
+
+    // メールアドレス
+    if (state.email != null) {
+      await SunmiPrinter.printText(
+        'Email: ${state.email!}',
+        style: SunmiStyle(
+          fontSize: SunmiFontSize.MD,
+          bold: true,
+        ),
+      );
+    }
+
+    // Webサイト
+    if (state.website != null) {
+      await SunmiPrinter.printText(
+        state.website!,
+        style: SunmiStyle(
+          fontSize: SunmiFontSize.MD,
+          bold: true,
+        ),
+      );
+    }
+
+    // SNS
+    for (final snsAccount in state.snsAccounts) {
+      await SunmiPrinter.printText(
+        '${snsAccount.type.name}: ${snsAccount.name}',
+        style: SunmiStyle(
+          fontSize: SunmiFontSize.MD,
+          bold: true,
+        ),
+      );
+    }
+
+    await SunmiPrinter.lineWrap(2);
+  }
 }
